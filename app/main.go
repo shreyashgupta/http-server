32b795fa1eac5b6ca7d6a0a3ea005474e447b917
shreyash <shreyashgupta88@gmail.com>
2025-06-22 16:26:57 +0530

    added path check

--- app/main.go
+++ app/main.go
@@ -68,7 +68,7 @@ func main() {
 	if len(extractPath(req[0])) == 1 {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	} else {
-		conn.Write([]byte("HTTP/1.1 404 NOT FOUND\r\n\r\n"))
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
 
 }
