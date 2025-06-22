8aec6ef2a06a8cc9d5d8d92624022824799b97a6
shreyash <shreyashgupta88@gmail.com>
2025-06-22 15:38:01 +0530

    added resp

--- app/main.go
+++ app/main.go
@@ -21,10 +21,12 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	resp := "HTTP/1.1 200 OK\r\n\r\n"
+	_, err = conn.Write([]byte(resp))
 }
