09f107a1fbbef3f63f51cea3346abd7f8be7480a
shreyash <shreyashgupta88@gmail.com>
2025-06-22 20:48:08 +0530

    header-parse

--- app/main.go
+++ app/main.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -13,7 +14,16 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
-func extractPath(req_line string) string {
+type RequestLineParser struct {
+}
+
+type HeaderParser struct {
+}
+
+type Bodyparser struct {
+}
+
+func (p *RequestLineParser) extractPath(req_line string) string {
 	// ^\w+ → Matches the HTTP method (e.g., GET, POST)
 	// \s+ → Space after method
 	// ([^\s]+) → Captures the path and query string, i.e., everything until next space
@@ -30,7 +40,28 @@ func extractPath(req_line string) string {
 	}
 }
 
-func extractEchoPath(path string) string {
+func (p *HeaderParser) parse(headersStr []string) (Headers, error) {
+	headers := Headers{}
+	headers.headers = make(map[string]string)
+	// fmt.Println(headersStr)
+	re := regexp.MustCompile(`([^\s]+):\s(.+)`)
+	for _, header := range headersStr {
+		if len(header) == 0 {
+			break
+		}
+
+		matches := re.FindStringSubmatch(header)
+		if len(matches) != 3 {
+			return Headers{}, fmt.Errorf("Failed to parse headers")
+		}
+		headers.headers[matches[1]] = matches[2]
+
+		// fmt.Println(headers.headers)
+	}
+	return headers, nil
+}
+
+func (p *RequestLineParser) extractEchoPath(path string) string {
 
 	re := regexp.MustCompile(`/echo/([^\s]+)`)
 
@@ -41,6 +72,161 @@ func extractEchoPath(path string) string {
 	} else {
 		return ""
 	}
+}
+
+func (p *RequestLineParser) extractUserAgent(path string) string {
+
+	re := regexp.MustCompile(`/echo/([^\s]+)`)
+
+	matches := re.FindStringSubmatch(path)
+
+	if len(matches) > 1 {
+		return matches[1]
+	} else {
+		return ""
+	}
+}
+
+type RequestParser struct {
+	reqLineParser RequestLineParser
+	headerParser  HeaderParser
+	bodyparser    Bodyparser
+	requestLines  []string
+}
+
+type RequestLine struct {
+	path string
+}
+
+func (r *RequestLine) getEchoPathIfEcho() (string, bool) {
+
+	re := regexp.MustCompile(`/echo/([^\s]+)`)
+
+	matches := re.FindStringSubmatch(r.path)
+
+	if len(matches) > 1 {
+		return matches[1], true
+	} else {
+		return "", false
+	}
+}
+
+func (r *RequestLine) isUserAgent() bool {
+
+	re := regexp.MustCompile(`/user-agent`)
+
+	match := re.Find([]byte(r.path))
+
+	return len(match) > 0
+}
+
+type Headers struct {
+	headers map[string]string
+}
+
+type Request struct {
+	requestLine RequestLine
+	headers     Headers
+	body        string
+}
+
+func NewRequestParser(conn net.Conn) *RequestParser {
+	reader := bufio.NewReader(conn)
+	var req []string
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			// Handle error (e.g., io.EOF for connection close)
+			break
+		}
+		req = append(req, strings.TrimSpace(line))
+		if strings.TrimSpace(line) == "" {
+			break // End of headers
+		}
+	}
+	return &RequestParser{requestLines: req}
+}
+
+func (p *RequestParser) parse() (Request, error) {
+	reqLinePath := p.reqLineParser.extractPath(p.requestLines[0])
+	headers, err := p.headerParser.parse(p.requestLines[1:])
+	if err != nil {
+		return Request{}, err
+	}
+	return Request{requestLine: RequestLine{path: reqLinePath}, headers: headers}, nil
+
+}
+
+func (p *RequestParser) getEchoPath() (string, error) {
+	path := p.reqLineParser.extractPath(p.requestLines[0])
+	echoPath := p.reqLineParser.extractEchoPath(path)
+
+	if echoPath != "" {
+		return echoPath, nil
+	}
+	return "", fmt.Errorf("Not an echo request")
+}
+
+func (p *RequestParser) getUserAgent() (string, error) {
+	path := p.reqLineParser.extractPath(p.requestLines[0])
+	echoPath := p.reqLineParser.extractEchoPath(path)
+
+	if echoPath != "" {
+		return echoPath, nil
+	}
+	return "", fmt.Errorf("Not an echo request")
+}
+
+type ResponseSelector struct {
+}
+
+func (s *ResponseSelector) getResponse(r Request) Response {
+	echoPath, isEcho := r.requestLine.getEchoPathIfEcho()
+	if isEcho {
+		return Response{code: 200,
+			codeDesc: "OK",
+			headers: map[string]string{
+				"Content-Length": strconv.Itoa(len(echoPath)),
+				"Content-Type":   "text/plain",
+			},
+			body: echoPath,
+		}
+	}
+	isUserAgent := r.requestLine.isUserAgent()
+	if isUserAgent {
+		return Response{code: 200,
+			codeDesc: "OK",
+			headers: map[string]string{
+				"Content-Length": strconv.Itoa(len(r.headers.headers["User-Agent"])),
+				"Content-Type":   "text/plain",
+			},
+			body: r.headers.headers["User-Agent"],
+		}
+	}
+
+	if len(r.requestLine.path) == 1 {
+		return Response{code: 200, codeDesc: "OK"}
+	} else {
+		return Response{code: 404, codeDesc: "Not Found"}
+	}
+
+}
+
+type Response struct {
+	code     int
+	codeDesc string
+	headers  map[string]string
+	body     string
+}
+
+func (resp *Response) getResponse() []byte {
+	rspLine := fmt.Sprintf("HTTP/1.1 %d %s", resp.code, resp.codeDesc)
+	headers := ""
+	for _key, _val := range resp.headers {
+		headers += fmt.Sprintf("%s: %s\r\n", _key, _val)
+	}
+	respStr := rspLine + "\r\n" + headers + "\r\n" + resp.body
+	return []byte(respStr)
 }
 
 func main() {
@@ -59,34 +245,30 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	// resp := "HTTP/1.1 200 OK\r\n\r\n"
-	// _, err = conn.Write([]byte(resp))
-	reader := bufio.NewReader(conn)
-	var req []string
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			// Handle error (e.g., io.EOF for connection close)
-			break
-		}
-		req = append(req, strings.TrimSpace(line))
-		if strings.TrimSpace(line) == "" {
-			break // End of headers
-		}
+	requestParser := NewRequestParser(conn)
+
+	req, err := requestParser.parse()
+	if err != nil {
+		fmt.Println("Error parsing request: ", err.Error())
+		os.Exit(1)
 	}
-	// for _, line := range req {
-	// 	fmt.Println(line)
+
+	responseSelector := ResponseSelector{}
+
+	response := responseSelector.getResponse(req)
+	conn.Write(response.getResponse())
+
+	// reqLineParser := RequestLineParser{req[0]}
+	// path := reqLineParser.extractPath(req[0])
+	// if len(path) == 1 {
+	// 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	// } else {
+	// 	echoPath := reqLineParser.extractEchoPath(path)
+	// 	if len(echoPath) == 0 {
+	// 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	// 	} else {
+	// 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoPath), echoPath)))
+	// 	}
 	// }
-	path := extractPath(req[0])
-	if len(path) == 1 {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else {
-		echoPath := extractEchoPath(path)
-		if len(echoPath) == 0 {
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		} else {
-			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoPath), echoPath)))
-		}
-	}
 
 }
