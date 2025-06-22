a78512b1c337dc90251954ae6eafc07b55692d3e
shreyash <shreyashgupta88@gmail.com>
2025-06-22 16:25:32 +0530

    added path check

--- app/main.go
+++ app/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"regexp"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+
+func extractPath(req_line string) string {
+	// ^\w+ → Matches the HTTP method (e.g., GET, POST)
+	// \s+ → Space after method
+	// ([^\s]+) → Captures the path and query string, i.e., everything until next space
+	// \s+HTTP/\d\.\d → Matches the HTTP version (e.g., HTTP/1.1)
+
+	re := regexp.MustCompile(`^\w+\s+([^\s]+)\s+HTTP/\d\.\d`)
+
+	matches := re.FindStringSubmatch(req_line)
+
+	if len(matches) > 1 {
+		return matches[1]
+	} else {
+		return ""
+	}
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -26,7 +46,29 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	// resp := "HTTP/1.1 200 OK\r\n\r\n"
+	// _, err = conn.Write([]byte(resp))
+	reader := bufio.NewReader(conn)
+	var req []string
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			// Handle error (e.g., io.EOF for connection close)
+			break
+		}
+		req = append(req, strings.TrimSpace(line))
+		if strings.TrimSpace(line) == "" {
+			break // End of headers
+		}
+	}
+	// for _, line := range req {
+	// 	fmt.Println(line)
+	// }
 
-	resp := "HTTP/1.1 200 OK\r\n\r\n"
-	_, err = conn.Write([]byte(resp))
+	if len(extractPath(req[0])) == 1 {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 NOT FOUND\r\n\r\n"))
+	}
+
 }
